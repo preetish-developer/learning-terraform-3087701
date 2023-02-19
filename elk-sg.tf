1fd0911bfb69e3dafabf7e98393eb501c37e947b
preetish-developer <125757743+preetish-developer@users.noreply.github.com>
2023-02-19 15:43:43 +0000

    1

--- elk-sg.tf
+++ elk-sg.tf
@@ -10,6 +10,18 @@ resource "aws_security_group_rule" "for_ssh" {
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
+  description       = "to be able to ssh"
+  
+  security_group_id = aws_security_group.dxm-logstash-sg.id
+}
+
+resource "aws_security_group_rule" "for_ssh" {
+  type              = "ingress"
+  from_port         = 9350
+  to_port           = 9350
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "for node js app"
   
   security_group_id = aws_security_group.dxm-logstash-sg.id
 }
