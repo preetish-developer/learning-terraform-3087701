aac0c09ba924047afe36b841b043f366084e9afa
preetish-developer <125757743+preetish-developer@users.noreply.github.com>
2023-02-19 16:11:07 +0000

    1

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,7 @@
-output "instance_id" {
-  count = 2
-  value = aws_instance.elk-node-1[count.index].id
+output "instance_id_1" {
+  value = aws_instance.elk-node-1[0].id
 }
 
+output "instance_id_1" {
+  value = aws_instance.elk-node-1[1].id
+}
