70daf4d19d0138335492dbec5b0c1d293652297a
preetish-developer <125757743+preetish-developer@users.noreply.github.com>
2023-02-19 15:26:59 +0000

    1

--- elk-ec2.tf
+++ elk-ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "elk-node-1" {
   instance_type = "t2.micro"
   key_name = "master-key"
   user_data = "${file("user_data.sh")}"
-  vpc_security_group_ids = dxm-logstash-sg.id
+  vpc_security_group_ids = aws_security_group.dxm-logstash-sg.id
 
   tags = {
     Name = "dxm-logstash-node-1"
--- elk-sg.tf
+++ elk-sg.tf
@@ -10,5 +10,5 @@ resource "aws_security_group_rule" "for_ssh" {
   to_port           = 22
   cidr_blocks       = "0.0.0.0/0"
   
-  security_group_id = dxm-logstash-sg.id
+  security_group_id = aws_security_group.dxm-logstash-sg.id
 }
