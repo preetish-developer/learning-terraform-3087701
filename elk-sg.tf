2f3da7e03037b5246e73edfbed680843c23e2b61
preetish-developer <125757743+preetish-developer@users.noreply.github.com>
2023-02-19 15:31:15 +0000

    1

--- elk-sg.tf
+++ elk-sg.tf
@@ -8,7 +8,8 @@ resource "aws_security_group_rule" "for_ssh" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
-  cidr_blocks       = "0.0.0.0/0"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   
   security_group_id = aws_security_group.dxm-logstash-sg.id
 }
