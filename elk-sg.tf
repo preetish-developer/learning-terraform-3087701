fda3a30592eac53a1a5067b8dcdf37cdaa4cb000
preetish-developer <125757743+preetish-developer@users.noreply.github.com>
2023-02-19 16:23:35 +0000

    1

--- elk-sg.tf
+++ elk-sg.tf
@@ -25,3 +25,14 @@ resource "aws_security_group_rule" "for_nodejs" {
   
   security_group_id = aws_security_group.dxm-logstash-sg.id
 }
+
+resource "aws_security_group_rule" "for_yum" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "for yum repos"
+  
+  security_group_id = aws_security_group.dxm-logstash-sg.id
+}
