14f6ee1e3b8a54cd91ad28c17d2a76418c08eea7
preetish-developer <125757743+preetish-developer@users.noreply.github.com>
2023-02-19 16:26:36 +0000

    2

--- elk-sg.tf
+++ elk-sg.tf
@@ -26,7 +26,7 @@ resource "aws_security_group_rule" "for_nodejs" {
   security_group_id = aws_security_group.dxm-logstash-sg.id
 }
 
-resource "aws_security_group_rule" "for_yum" {
+resource "aws_security_group_rule" "for_yum_repos" {
   type              = "egress"
   from_port         = 443
   to_port           = 443
