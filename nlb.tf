f8af83206430dd92464a270f802e86eec3fc045c
preetish-developer <125757743+preetish-developer@users.noreply.github.com>
2023-02-21 19:45:29 +0000

    3

--- nlb.tf
+++ nlb.tf
@@ -18,7 +18,7 @@ resource "aws_lb_listener" "https-listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.front_end.arn
+    target_group_arn = aws_lb_target_group.logstash-nlb-tg.arn
   }
 }
 
