03744b205d2021f08e2dc11e6686dc1d2d19a86d
preetish-developer <125757743+preetish-developer@users.noreply.github.com>
2023-03-02 14:25:07 +0000

    2

--- elk-ec2.tf
+++ elk-ec2.tf
@@ -1,25 +1,25 @@
-resource "aws_instance" "elk-node-1" {
+# resource "aws_instance" "elk-node-1" {
 
-  count = 2
+#   count = 2
 
-  ami = "ami-05c96317a6278cfaa"
-  instance_type = "t2.micro"
-  key_name = "master-key"
-  user_data = "${file("user_data.sh")}"
-  vpc_security_group_ids = [aws_security_group.dxm-logstash-sg.id]
+#   ami = "ami-05c96317a6278cfaa"
+#   instance_type = "t2.micro"
+#   key_name = "master-key"
+#   user_data = "${file("user_data.sh")}"
+#   vpc_security_group_ids = [aws_security_group.dxm-logstash-sg.id]
 
-  tags = {
-    Name = var.node_names[count.index]
-  }
+#   tags = {
+#     Name = var.node_names[count.index]
+#   }
 
-}
+# }
 
 resource "aws_instance" "for_api_execute" {
 
   ami = "ami-05c96317a6278cfaa"
   
   # instance_type = "t2.micro"
-  instance_type = "t3a.small"
+  instance_type = "t3a.medium"
 
   key_name = "master-key"
   vpc_security_group_ids = [aws_security_group.dxm-logstash-sg.id]
--- nlb.tf
+++ nlb.tf
@@ -1,42 +1,42 @@
-resource "aws_lb" "logstash-nlb" {
-  name               = "logstash-nlb"
-  internal           = true
-  load_balancer_type = "network"
-  subnets            = ["subnet-0db9f6ca7703275f7","subnet-0a421dd631424f498","subnet-0813f4a73d59f8efe"]
+# resource "aws_lb" "logstash-nlb" {
+#   name               = "logstash-nlb"
+#   internal           = true
+#   load_balancer_type = "network"
+#   subnets            = ["subnet-0db9f6ca7703275f7","subnet-0a421dd631424f498","subnet-0813f4a73d59f8efe"]
 
-  enable_deletion_protection = false
+#   enable_deletion_protection = false
 
-}
+# }
 
-resource "aws_lb_listener" "https-listener" {
-  load_balancer_arn = aws_lb.logstash-nlb.arn
-  port              = "443"
-  protocol          = "TLS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+# resource "aws_lb_listener" "https-listener" {
+#   load_balancer_arn = aws_lb.logstash-nlb.arn
+#   port              = "443"
+#   protocol          = "TLS"
+#   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   
-  certificate_arn   = "arn:aws:acm:eu-west-2:317138350195:certificate/e593dcd3-fae8-4089-98a3-94b63d065f85"
+#   certificate_arn   = "arn:aws:acm:eu-west-2:317138350195:certificate/e593dcd3-fae8-4089-98a3-94b63d065f85"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.logstash-nlb-tg.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.logstash-nlb-tg.arn
+#   }
+# }
 
-resource "aws_lb_target_group" "logstash-nlb-tg" {
-  name     = "logstash-nlb-tg"
-  port     = 9350
-  protocol = "TCP"
-  vpc_id   = "vpc-05fcf95ab8a0060c7"
-}
+# resource "aws_lb_target_group" "logstash-nlb-tg" {
+#   name     = "logstash-nlb-tg"
+#   port     = 9350
+#   protocol = "TCP"
+#   vpc_id   = "vpc-05fcf95ab8a0060c7"
+# }
 
-resource "aws_lb_target_group_attachment" "node-1" {
-  target_group_arn = aws_lb_target_group.logstash-nlb-tg.arn
-  target_id        = aws_instance.elk-node-1[0].id
-  port             = 9350
-}
+# resource "aws_lb_target_group_attachment" "node-1" {
+#   target_group_arn = aws_lb_target_group.logstash-nlb-tg.arn
+#   target_id        = aws_instance.elk-node-1[0].id
+#   port             = 9350
+# }
 
-resource "aws_lb_target_group_attachment" "node-2" {
-  target_group_arn = aws_lb_target_group.logstash-nlb-tg.arn
-  target_id        = aws_instance.elk-node-1[1].id
-  port             = 9350
-}
+# resource "aws_lb_target_group_attachment" "node-2" {
+#   target_group_arn = aws_lb_target_group.logstash-nlb-tg.arn
+#   target_id        = aws_instance.elk-node-1[1].id
+#   port             = 9350
+# }
