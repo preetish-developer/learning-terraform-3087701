f36aa45990674fe6708d85d28020f90c59a549e2
preetish-developer <125757743+preetish-developer@users.noreply.github.com>
2023-03-02 14:26:51 +0000

    sdf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "instance_id_1" {
-  value = aws_instance.elk-node-1[0].id
-}
+# output "instance_id_1" {
+#   value = aws_instance.elk-node-1[0].id
+# }
 
-output "instance_id_2" {
-  value = aws_instance.elk-node-1[1].id
-}
+# output "instance_id_2" {
+#   value = aws_instance.elk-node-1[1].id
+# }
 
-output "nlb_dns" {
-  value = aws_lb.logstash-nlb.dns_name
-}
+# output "nlb_dns" {
+#   value = aws_lb.logstash-nlb.dns_name
+# }
